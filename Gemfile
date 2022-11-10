9351411e86dd9bcbc9dd7ae259e667bb7f7388ec
Blake Rosenberg <blakerosenberg@macroplant.com>
2022-11-10 11:32:45 -0500

    Revert "Adds coverage reporting for Coveralls"

--- Gemfile
+++ Gemfile
@@ -5,9 +5,6 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in ocr4pdf.gemspec
 gemspec
 
-gem "simplecov", ">= 0.18.1", "< 0.22.0"
-gem "simplecov-lcov", "~> 0.8.0"
-
 gem "rake", "~> 13.0"
 
 gem "rspec", "~> 3.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,5 @@
 # frozen_string_literal: true
 
-require "simplecov"
-
-SimpleCov.start do
-  require "simplecov-lcov"
-
-  SimpleCov::Formatter::LcovFormatter.config do |c|
-    c.report_with_single_file = true
-    c.single_report_path = "coverage/lcov.info"
-  end
-
-  formatter SimpleCov::Formatter::LcovFormatter
-
-  add_filter do |source_file|
-    source_file.filename.include?("spec") && !source_file.filename.include?("fixture")
-  end
-  add_filter %r{/.bundle/}
-end
-
 require "ocr4pdf"
 
 RSpec.configure do |config|
