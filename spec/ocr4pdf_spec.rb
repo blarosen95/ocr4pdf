eaf8492f8635ec475a21671456423d6bf3738fbb
Blake Rosenberg <blakerosenberg@macroplant.com>
2022-11-10 11:06:41 -0500

    test: re-enables all tests

--- spec/ocr4pdf_spec.rb
+++ spec/ocr4pdf_spec.rb
@@ -21,35 +21,34 @@ RSpec.describe Ocr4pdf do
     File.delete(ocr_file)
   end
 
-  # TODO: Disabling these last tests to speed up workflow runs while configuration still in progress:
-  # it "can create a PDF with OCR text from a PDF with regular embedded text" do
-  #   ocr_blob = Ocr4pdf.new("spec/fixtures/w4-page-2.pdf").create_ocr_pdf
-  #   File.binwrite("spec/fixtures/w4-page-2.ocr.pdf", ocr_blob)
-  #   ocr_file = File.expand_path("spec/fixtures/w4-page-2.ocr.pdf")
-  #
-  #   first_line_text = Open3.capture2("pdftotext -f 1 -l 1 -r 300 #{ocr_file} - | head -n 1")[0].strip
-  #   expect(first_line_text).to start_with "Form W-4 (2022)"
-  #
-  #   # Delete the file
-  #   File.delete(ocr_file)
-  # end
-  #
-  # it "can create a PDF from a multi-page rasterized PDF" do
-  #   ocr_blob = Ocr4pdf.new("spec/fixtures/form-w4.pdf").create_ocr_pdf
-  #   File.binwrite("spec/fixtures/form-w4.ocr.pdf", ocr_blob)
-  #   ocr_file = File.expand_path("spec/fixtures/form-w4.ocr.pdf")
-  #
-  #   third_page_first_text_line = Open3.capture2("pdftotext -f 3 -l 3 -r 300 #{ocr_file} - | head -n 3")[0].strip
-  #   third_page_first_text_line = third_page_first_text_line.tr("\n", " ").gsub(/\s{2,}/, " ")
-  #   # expect(third_page_first_text_line).to start_with "Form W-4 (2022) Page 3"
-  #   # TODO: This used to work with the exact same Tesseract model, but now it's not seeing the `-` in `W-4` or the ` ` in `Page 3`...
-  #   expect(third_page_first_text_line).to start_with("Form W-4 (2022) Page 3").or start_with "Form W4 (2022) Page3"
-  #   # TODO: If relevant, it's worth noting that using `--oem 1` will see the `-` in `W-4` AND the space in `Page 3` still...
-  #
-  #   # TODO: Note, using this same fixture file, the first bits of text in page 1 are OCR'd completely wrong: "Form W=4" or slightly worse...
-  #   # TODO: Really need to train my own LSTM model for it to perform better than this...
-  #
-  #   # Delete the file
-  #   File.delete(ocr_file)
-  # end
+  it "can create a PDF with OCR text from a PDF with regular embedded text" do
+    ocr_blob = Ocr4pdf.new("spec/fixtures/w4-page-2.pdf").create_ocr_pdf
+    File.binwrite("spec/fixtures/w4-page-2.ocr.pdf", ocr_blob)
+    ocr_file = File.expand_path("spec/fixtures/w4-page-2.ocr.pdf")
+
+    first_line_text = Open3.capture2("pdftotext -f 1 -l 1 -r 300 #{ocr_file} - | head -n 1")[0].strip
+    expect(first_line_text).to start_with "Form W-4 (2022)"
+
+    # Delete the file
+    File.delete(ocr_file)
+  end
+
+  it "can create a PDF from a multi-page rasterized PDF" do
+    ocr_blob = Ocr4pdf.new("spec/fixtures/form-w4.pdf").create_ocr_pdf
+    File.binwrite("spec/fixtures/form-w4.ocr.pdf", ocr_blob)
+    ocr_file = File.expand_path("spec/fixtures/form-w4.ocr.pdf")
+
+    third_page_first_text_line = Open3.capture2("pdftotext -f 3 -l 3 -r 300 #{ocr_file} - | head -n 3")[0].strip
+    third_page_first_text_line = third_page_first_text_line.tr("\n", " ").gsub(/\s{2,}/, " ")
+    # expect(third_page_first_text_line).to start_with "Form W-4 (2022) Page 3"
+    # TODO: This used to work with the exact same Tesseract model, but now it's not seeing the `-` in `W-4` or the ` ` in `Page 3`...
+    expect(third_page_first_text_line).to start_with("Form W-4 (2022) Page 3").or start_with "Form W4 (2022) Page3"
+    # TODO: If relevant, it's worth noting that using `--oem 1` will see the `-` in `W-4` AND the space in `Page 3` still...
+
+    # TODO: Note, using this same fixture file, the first bits of text in page 1 are OCR'd completely wrong: "Form W=4" or slightly worse...
+    # TODO: Really need to train my own LSTM model for it to perform better than this...
+
+    # Delete the file
+    File.delete(ocr_file)
+  end
 end
